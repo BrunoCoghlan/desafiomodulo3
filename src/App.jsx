b51c4629d736db388a0f1eef9f13c173b35b93f7
Bruno Coghlan <brunocoghlan@gmail.com>
2024-03-13 23:43:55 -0300

    MyFirstCommit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import BaseColaboradores from './js/BaseColaboradores.js'
+
+const App = () => {
+  const colaboradores = BaseColaboradores
+  console.log(colaboradores)
+  return (
+    <div>asd</div>
+  )
+}
+
+export default App
---
+++ src/components/Formulario.jsx
@@ -0,0 +1,14 @@
+const Formulario = () => {
+  return (
+    <>
+      <form action=''>
+        <input type='text' />
+        <input type='email' />
+        <input type='text' />
+
+      </form>
+    </>
+  )
+}
+
+export default Formulario
